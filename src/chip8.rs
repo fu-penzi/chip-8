a4b2e8d9e5dfc694b81b98a459763629ca08d372
Piotr Nowak <npiotrek44@gmail.com>
2025-09-26 00:30:33 +0200

    chore: Add comments for instructions

--- src/chip8.rs
+++ src/chip8.rs
@@ -173,39 +173,38 @@ impl Chip8 {
         };
     }
 
-    // CLS
-    // Clear display
+    /// `CLS`
+    /// Clear display
     fn op_00e0(&mut self) {
         self.video = [false; DISP_WIDTH * DISP_HEIGHT];
-        // for col in self.video.iter_mut() {
-        //     *col = 0;
-        // }
     }
 
-    // RET
-    // Return from subroutine
-    // Pop address from stack and set PC as popped address
+    /// `RET`
+    /// Return from subroutine.
+    /// Pop address from stack and set PC to popped address.
     fn op_00ee(&mut self) {
         self.sp -= 1;
         self.pc = self.stack[self.sp as usize];
     }
 
-    // JP addr
-    // Jump to address NNN
+    /// `JMP NNN`
+    /// Jump to address NNN.
+    /// PC = NNN
     fn op_1nnn(&mut self) {
         self.pc = self.opcode & 0x0FFF;
     }
 
-    // CALL NNN
-    // Put current PC on stack; set PC to NNN
+    /// `CALL NNN`
+    /// Put current PC on stack
+    /// PC = NNN
     fn op_2nnn(&mut self) {
         self.stack[self.sp as usize] = self.pc;
         self.sp += 1;
         self.pc = self.opcode & 0x0FFF;
     }
 
-    // SE Vx, NN
-    // Skip next operation if Vx == NN
+    /// `SE Vx, NN`
+    /// Skip next operation if Vx == NN.
     fn op_3xnn(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let nn: u8 = (self.opcode & 0x00FF) as u8;
@@ -214,8 +213,8 @@ impl Chip8 {
         }
     }
 
-    // SNE Vx, NN
-    // Skip next operation if Vx != NN
+    /// `SNE Vx, NN`
+    /// Skip next operation if Vx != NN.
     fn op_4xnn(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let nn: u8 = (self.opcode & 0x00FF) as u8;
@@ -224,8 +223,8 @@ impl Chip8 {
         }
     }
 
-    // SE Vx, NN
-    // Skip next operation if Vx == Vy
+    /// `SE Vx, VY`
+    /// Skip next operation if Vx == Vy.
     fn op_5xy0(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
@@ -234,56 +233,56 @@ impl Chip8 {
         }
     }
 
-    // LD Vx, NN
-    // Set Vx to value NN
+    /// `LD Vx, NN`
+    /// Vx = NN
     fn op_6xnn(&mut self) {
         let x: usize = (self.opcode as usize & 0x0F00) >> 8;
         let nn: u8 = (self.opcode & 0xFF) as u8;
         self.registers[x] = nn;
     }
 
-    // ADD Vx, NN
-    // Add NN to register Vx
+    /// `ADD Vx, NN`
+    /// Vx = Vx + NN
     fn op_7xnn(&mut self) {
         let x: usize = (self.opcode as usize & 0x0F00) >> 8;
         let nn: u8 = (self.opcode & 0xFF) as u8;
         self.registers[x] = self.registers[x].wrapping_add(nn);
     }
 
-    // LD Vx, Vy
-    // Set Vx to value of Vy
+    /// `LD Vx, Vy`
+    /// Vy = Vx
     fn op_8xy0(&mut self) {
         let v_x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let v_y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
         self.registers[v_x] = self.registers[v_y];
     }
 
-    // OR Vx, Vy
-    // Set Vx to Vx OR Vy
+    /// `OR Vx, Vy`
+    /// Vx = Vx | Vy
     fn op_8xy1(&mut self) {
         let v_x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let v_y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
         self.registers[v_x] |= self.registers[v_y];
     }
 
-    // AND Vx, Vy
-    // Set Vx to Vx AND Vy
+    /// `AND Vx, Vy`
+    /// Vx = Vx & Vy
     fn op_8xy2(&mut self) {
         let v_x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let v_y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
         self.registers[v_x] &= self.registers[v_y];
     }
 
-    // XOR Vx, Vy
-    // Set Vx to Vx XOR Vy
+    /// `XOR Vx, Vy`
+    /// Vx = Vx ^ Vy
     fn op_8xy3(&mut self) {
         let v_x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let v_y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
         self.registers[v_x] ^= self.registers[v_y];
     }
 
-    // ADD Vx, Vy
-    // Set Vx to Vx + Vy
+    /// `ADD Vx, Vy`
+    /// Vx = Vx + Vy
     fn op_8xy4(&mut self) {
         let v_x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let v_y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
@@ -292,8 +291,8 @@ impl Chip8 {
         self.registers[v_x] = new_vx;
     }
 
-    // SUB Vx, Vy
-    // Set Vx to Vx - Vy
+    /// `SUB Vx, Vy`
+    /// Vx = Vx - Vy
     fn op_8xy5(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
@@ -302,16 +301,18 @@ impl Chip8 {
         self.registers[x] = new_vx;
     }
 
-    // SHR Vx, Vy
-    // Shift right by 1, !!!! Ambiguous instruction!
+    /// `SHR Vx, Vy`
+    /// Vx = Vx >> 1
+    /// Vy ignored
     fn op_8xy6(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         self.registers[0xF] = self.registers[x] & 1;
         self.registers[x] >>= 1;
     }
 
-    // SUB Vy, Vx
-    // Set Vx to Vy - Vxdw
+    /// `SUBN Vy, Vx`
+    /// Vx to Vy - Vx
+    /// if underflows VF = 0, else VF = 1
     fn op_8xy7(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
@@ -320,7 +321,9 @@ impl Chip8 {
         self.registers[x] = new_vx;
     }
 
-    // SHL VX, VY
+    /// `SHL Vx, VY`
+    /// VF = most significant bit of Vx;
+    /// Vx = Vx << 1
     fn op_8xye(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
 
@@ -328,8 +331,8 @@ impl Chip8 {
         self.registers[x] <<= 1;
     }
 
-    // SNE Vx, Vy
-    // Skip next instruction if Vx != Vy
+    /// `SNE Vx, Vy`
+    /// Skip next instruction if Vx != Vy.
     fn op_9xy0(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
@@ -338,19 +341,21 @@ impl Chip8 {
         }
     }
 
-    // LD I, NNN
-    // Set index register I to value NNN
+    /// `LD I, NNN`
+    /// I = NNN
+    fn op_annn(&mut self) {
+        self.i = self.opcode & 0x0FFF;
+    }
+
+    /// `JMP V0, NNN`
+    /// PC = V0 + NNN
     fn op_bnnn(&mut self) {
         let nnn = self.opcode & 0xFFF;
         self.pc = (self.registers[0] as u16) + nnn;
     }
 
-    // LD I, NNN
-    // Set index register I to value NNN
-    fn op_annn(&mut self) {
-        self.i = self.opcode & 0x0FFF;
-    }
-
+    /// `RND Vx, NN`
+    /// Vx = random byte & NN
     fn op_cxnn(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let nn: u8 = (self.opcode & 0x00FF) as u8;
@@ -358,8 +363,8 @@ impl Chip8 {
         self.registers[x] = nn & rng;
     }
 
-    // DRW Vx, Vy, nibble
-    // Draw N bytes sized sprite from RAM[I] to display at [Vx][Vy].
+    /// `DRW Vx, Vy, N`
+    /// Draw N-byte sized sprite from `RAM[I]` to display at `[Vx][Vy]`.
     fn op_dxyn(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let y: usize = ((self.opcode & 0x00F0) >> 4) as usize;
@@ -400,8 +405,8 @@ impl Chip8 {
         self.registers[0xF] = if collision { 1 } else { 0 };
     }
 
-    // SKP Vx
-    // Skip next instruction if key with value of Vx is pressed
+    /// `SKP Vx`
+    /// Skip next instruction if key with value of Vx is pressed
     fn op_ex9e(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         if self.keypad[self.registers[x] as usize] > 0 {
@@ -409,8 +414,8 @@ impl Chip8 {
         }
     }
 
-    // SKNP Vx
-    // Skip next instruction if key with value of Vx is not pressed
+    /// `SKNP Vx`
+    /// Skip next instruction if key with value of Vx is not pressed
     fn op_exa1(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         if self.keypad[self.registers[x] as usize] == 0 {
@@ -418,11 +423,15 @@ impl Chip8 {
         }
     }
 
+    /// `LD Vx, DT`
+    /// DT = Vx (set delay timer to Vx)
     fn op_fx07(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         self.registers[x] = self.dt;
     }
 
+    /// `LD Vx, KEY`
+    /// Wait for KEY press and store KEY value in Vx
     fn op_fx0a(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
 
@@ -433,49 +442,63 @@ impl Chip8 {
         }
     }
 
+    /// `LD DT, Vx`
+    /// DT = Vx (set delay timer to Vx)
     fn op_fx15(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         self.dt = self.registers[x];
     }
 
+    /// `LD ST, Vx`
+    /// ST = Vx (set sount timer to Vx)
     fn op_fx18(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         self.st = self.registers[x];
     }
 
-    // ADD I, Vx
-    // Add value of Vx to I
+    /// `ADD I, Vx`
+    /// Vx = Vx + I
     fn op_fx1e(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         self.i = self.i.wrapping_add(self.registers[x] as u16);
     }
 
+    /// `LD I, FONT(Vx)`
+    /// Load 5-byte Font character representing Vx to I.
     fn op_fx29(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         self.i = self.registers[x] as u16 * 5;
     }
 
+    /// `BCD Vx`
+    /// Decode Vx to decimal.
+    /// Set `RAM[I], RAM[I+1], RAM[I+2]` to hundreds, tens and ones.
+    /// 
+    /// ex. for `Vx = 123` => `RAM[I] = 1; RAM[I+1] = 2; RAM[I+2] = 3`
     fn op_fx33(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         let v_x: u8 = self.registers[x];
 
-        let digit_3: u8 = v_x % 10;
-        let digit_2: u8 = (v_x % 100 - digit_3) / 10;
-        let digit_1: u8 = (v_x - (digit_2 * 10) - digit_3) / 100;
+        let ones: u8 = v_x % 10;
+        let tens: u8 = (v_x % 100 - ones) / 10;
+        let hundreds: u8 = (v_x - (tens * 10) - ones) / 100;
 
-        self.ram[self.i as usize] = digit_1;
-        self.ram[(self.i + 1) as usize] = digit_2;
-        self.ram[(self.i + 2) as usize] = digit_3;
+        self.ram[self.i as usize] = hundreds;
+        self.ram[(self.i + 1) as usize] = tens;
+        self.ram[(self.i + 2) as usize] = ones;
     }
 
+    /// `LD [I], VX`
+    /// Load values of registers from V0 to Vx to memory starting at address I.
     fn op_fx55(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         for i in 0..=x {
             self.ram[(self.i + i as u16) as usize] = self.registers[i];
         }
-        self.i = self.i.wrapping_add((x + 1) as u16);
     }
 
+    /// `LD VX, [I]`
+    /// Load values from memory starting at address I to registers from V0 to Vx.
     fn op_fx65(&mut self) {
         let x: usize = ((self.opcode & 0x0F00) >> 8) as usize;
         for i in 0..=x {
